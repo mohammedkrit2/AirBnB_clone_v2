c8b4842e7e4db2d5fe2e602d26f87a444caf4ab1
mohammed krit <hj72475@gmail.com>
2024-07-14 23:17:39 +0000

    fix port errore

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,25 @@
-#!/usr/bin/env bash
-# Script to set up web servers for deployment of web_static
-
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo chown -R root:root /data/
-
-sudo sed -i '/server_name _;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
-
-exit 0
